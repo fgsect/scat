b918b1fbcaf792a15eae9602e270e8ba327ee877
Shinjo Park <peremen@gmail.com>
2018-12-16 22:37:30 +0100

    More DSDS related definitions and fixes

--- parser/qualcommparser.py
+++ parser/qualcommparser.py
@@ -211,6 +211,7 @@ class QualcommParser:
             return
 
     def stop_diag(self):
+        self.handler.read(0x1000)
         print('-------- stop diag --------')
         # Static event reporting Disable
         self._write_then_read_discard(struct.pack('<BB', diagcmd.DIAG_EVENT_REPORT_F, 0x00), False, True)
@@ -267,20 +268,20 @@ class QualcommParser:
 
     def parse_gsm_dsds_fcch(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_fcch(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_fcch(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_sch(self, pkt_ts, pkt, radio_id):
         self.parse_gsm_fcch(pkt_ts, pkt, radio_id)
 
     def parse_gsm_dsds_sch(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_sch(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_sch(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_l1_new_burst_metric(self, pkt_ts, pkt, radio_id):
         version = pkt[0]
@@ -346,10 +347,10 @@ class QualcommParser:
 
     def parse_gsm_dsds_l1_burst_metric(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_l1_burst_metric(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_l1_burst_metric(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_l1_surround_cell_ba(self, pkt_ts, pkt, radio_id):
         num_cells = pkt[0]
@@ -369,10 +370,10 @@ class QualcommParser:
 
     def parse_gsm_dsds_l1_surround_cell_ba(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_l1_surround_cell_ba(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_l1_surround_cell_ba(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_l1_serv_aux_meas(self, pkt_ts, pkt, radio_id):
         interim = struct.unpack('<hB', pkt[0:3])
@@ -383,10 +384,10 @@ class QualcommParser:
 
     def parse_gsm_dsds_l1_serv_aux_meas(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_l1_serv_aux_meas(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_l1_serv_aux_meas(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_l1_neig_aux_meas(self, pkt_ts, pkt, radio_id):
         num_cells = pkt[0]
@@ -403,10 +404,10 @@ class QualcommParser:
 
     def parse_gsm_dsds_l1_neig_aux_meas(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_l1_neig_aux_meas(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_l1_neig_aux_meas(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_cell_info(self, pkt_ts, pkt, radio_id):
         arfcn_band = (pkt[1] << 8) | pkt[0]
@@ -420,10 +421,10 @@ class QualcommParser:
 
     def parse_gsm_dsds_cell_info(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_cell_info(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_cell_info(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gsm_rr(self, pkt_ts, pkt, radio_id):
         chan_type_dir = pkt[0]
@@ -495,10 +496,10 @@ class QualcommParser:
 
     def parse_gsm_dsds_rr(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_gsm_rr(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_gsm_rr(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     def parse_gprs_mac(self, pkt_ts, pkt, radio_id):
         print("Unhandled XDM Header 0x5226: GPRS MAC Packet")
@@ -559,8 +560,8 @@ class QualcommParser:
         self.writeCP(gsmtap_hdr + l3_message, radio_id)
 
     # 3G
-    def parse_wcdma_search_cell_reselection(self, pkt_ts, pkt, radio_id):
-        num_wcdma_cells = pkt[0]
+    def parse_wcdma_search_cell_reselection_v0(self, pkt_ts, pkt, radio_id):
+        num_wcdma_cells = pkt[0] & 0x3f # lower 6b
         num_gsm_cells = pkt[1] # TODO: check if num_gsm_cells > 0
 
         print('Radio {}: 3G Cell: # cells {}'.format(radio_id, num_wcdma_cells))
@@ -573,23 +574,47 @@ class QualcommParser:
             n_cell_rank_rscp = cell_pkt_vals[3]
             n_cell_ecio = cell_pkt_vals[4]
             n_cell_rank_ecio = cell_pkt_vals[5]
-            print('Radio {}: Cell {}: UARFCN {}, PSC {}, RSCP {}, Ec/Io {:.2f}'.format(radio_id, i, n_cell_uarfcn, n_cell_psc, n_cell_rscp - 21, n_cell_ecio / 2))
+            print('Radio {}: Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'.format(radio_id, i, n_cell_uarfcn, n_cell_psc, n_cell_rscp - 21, n_cell_ecio / 2))
+
+    def parse_wcdma_search_cell_reselection_v2(self, pkt_ts, pkt, radio_id):
+        num_wcdma_cells = pkt[0] & 0x3f # lower 6b
+        num_gsm_cells = pkt[1] # TODO: check if num_gsm_cells > 0
+
+        print('Radio {}: 3G Cell: # cells {}'.format(radio_id, num_wcdma_cells))
+        for i in range(num_wcdma_cells):
+            cell_pkt = pkt[7 + 16 * i:7 + 16 * (i + 1)]
+            cell_pkt_vals = struct.unpack('<HHbhbh', cell_pkt[:10])
+            n_cell_uarfcn = cell_pkt_vals[0]
+            n_cell_psc = cell_pkt_vals[1]
+            n_cell_rscp = cell_pkt_vals[2]
+            n_cell_rank_rscp = cell_pkt_vals[3]
+            n_cell_ecio = cell_pkt_vals[4]
+            n_cell_rank_ecio = cell_pkt_vals[5]
+            print('Radio {}: Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'.format(radio_id, i, n_cell_uarfcn, n_cell_psc, n_cell_rscp - 21, n_cell_ecio / 2))
+
+    def parse_wcdma_search_cell_reselection(self, pkt_ts, pkt, radio_id):
+        pkt_version = (pkt[0] >> 6) # upper 2b
+
+        if pkt_version == 0:
+            self.parse_wcdma_search_cell_reselection_v0(pkt_ts, pkt, radio_id)
+        elif pkt_version == 2:
+            self.parse_wcdma_search_cell_reselection_v2(pkt_ts, pkt, radio_id)
+        else:
+            print('Unsupported WCDMA search cell reselection version {}'.format(pkt_version))
+            util.xxd(pkt)
 
     def parse_wcdma_cell_id(self, pkt_ts, pkt, radio_id):
-        if len(pkt) < 12:
-            return
-
-        result = struct.unpack('<LLL', pkt[0:12])
+        result = struct.unpack('<LLLHHHBBBBBBLL', pkt[0:32])
+        # UARFCN UL, UARFCN DL, CID, URA_ID, FLAGS, PSC, PLMN_ID, LAC, RAC
+        # PSC needs to be >>4'ed
         self.umts_last_uarfcn_ul[radio_id] = result[0] | (1 << 14)
         self.umts_last_uarfcn_dl[radio_id] = result[1]
         self.umts_last_cell_id[radio_id] = result[2] & 0x7fff
 
     def parse_wcdma_rrc(self, pkt_ts, pkt, radio_id):
-        channel_type = pkt[0]
-        rbid = pkt[1]
-        msg_len = pkt[2] | (pkt[3] << 8)
+        channel_type, rbid, msg_len = struct.unpack('<BBH', pkt[0:4])
         sib_class = -1
-
+        arfcn = 0
         msg_content = b''
 
         channel_type_map = {
@@ -602,22 +627,14 @@ class QualcommParser:
                 6: util.gsmtap_umts_rrc_types.PCCH,
                 7: util.gsmtap_umts_rrc_types.MCCH,
                 8: util.gsmtap_umts_rrc_types.MSCH,
-                9: util.gsmtap_umts_rrc_types.BCCH_BCH,
                 10: util.gsmtap_umts_rrc_types.System_Information_Container,
+        }
+
+        channel_type_map_extended_type = {
+                9: util.gsmtap_umts_rrc_types.BCCH_BCH, # Extension SIBs
                 0xFE: util.gsmtap_umts_rrc_types.BCCH_BCH, # Decoded
                 0xFF: util.gsmtap_umts_rrc_types.BCCH_FACH # Decoded
-                }
-
-        subtype = 0
-        try:
-            subtype = channel_type_map[pkt[0]]
-        except KeyError:
-            print("Unknown WCDMA RRC channel type {}".format(pkt[0]))
-            util.xxd(pkt)
-
-        arfcn = self.umts_last_uarfcn_dl[radio_id]
-        if channel_type == 0 or channel_type == 1:
-            arfcn = self.umts_last_uarfcn_ul[radio_id]
+        }
 
         sib_type_map = {
                 0: util.gsmtap_umts_rrc_types.MasterInformationBlock,
@@ -655,16 +672,98 @@ class QualcommParser:
                 # Extension SIB
                 66: util.gsmtap_umts_rrc_types.SysInfoType11bis,
                 67: util.gsmtap_umts_rrc_types.SysInfoType19
-                }
+        }
 
-        if channel_type == 0xFE or channel_type == 0xFF or channel_type == 9:
-            try:
-                subtype = sib_type_map[pkt[4]]
-            except KeyError:
-                print("Unknown WCDMA SIB Class {}".format(pkt[4]))
-            msg_content = pkt[5:]
-        else:
+        channel_type_map_new = {
+                0x80: util.gsmtap_umts_rrc_types.UL_CCCH,
+                0x81: util.gsmtap_umts_rrc_types.UL_DCCH,
+                0x82: util.gsmtap_umts_rrc_types.DL_CCCH,
+                0x83: util.gsmtap_umts_rrc_types.DL_DCCH,
+                0x84: util.gsmtap_umts_rrc_types.BCCH_BCH, # Encoded
+                0x85: util.gsmtap_umts_rrc_types.BCCH_FACH, # Encoded
+                0x86: util.gsmtap_umts_rrc_types.PCCH,
+                0x87: util.gsmtap_umts_rrc_types.MCCH,
+                0x88: util.gsmtap_umts_rrc_types.MSCH,
+        }
+        channel_type_map_new_extended_type = {
+                0x89: util.gsmtap_umts_rrc_types.BCCH_BCH, # Extension SIBs
+                0xF0: util.gsmtap_umts_rrc_types.BCCH_BCH, # Decoded
+        }
+        sib_type_map_new = {
+                0: util.gsmtap_umts_rrc_types.MasterInformationBlock,
+                1: util.gsmtap_umts_rrc_types.SysInfoType1,
+                2: util.gsmtap_umts_rrc_types.SysInfoType2,
+                3: util.gsmtap_umts_rrc_types.SysInfoType3,
+                4: util.gsmtap_umts_rrc_types.SysInfoType4,
+                5: util.gsmtap_umts_rrc_types.SysInfoType5,
+                6: util.gsmtap_umts_rrc_types.SysInfoType6,
+                7: util.gsmtap_umts_rrc_types.SysInfoType7,
+                8: util.gsmtap_umts_rrc_types.SysInfoType8,
+                9: util.gsmtap_umts_rrc_types.SysInfoType9,
+                10: util.gsmtap_umts_rrc_types.SysInfoType10,
+                11: util.gsmtap_umts_rrc_types.SysInfoType11,
+                12: util.gsmtap_umts_rrc_types.SysInfoType12,
+                13: util.gsmtap_umts_rrc_types.SysInfoType13,
+                14: util.gsmtap_umts_rrc_types.SysInfoType13_1,
+                15: util.gsmtap_umts_rrc_types.SysInfoType13_2,
+                16: util.gsmtap_umts_rrc_types.SysInfoType13_3,
+                17: util.gsmtap_umts_rrc_types.SysInfoType13_4,
+                18: util.gsmtap_umts_rrc_types.SysInfoType14,
+                19: util.gsmtap_umts_rrc_types.SysInfoType15,
+                20: util.gsmtap_umts_rrc_types.SysInfoType15_1,
+                21: util.gsmtap_umts_rrc_types.SysInfoType15_2,
+                22: util.gsmtap_umts_rrc_types.SysInfoType15_3,
+                23: util.gsmtap_umts_rrc_types.SysInfoType16,
+                24: util.gsmtap_umts_rrc_types.SysInfoType17,
+                25: util.gsmtap_umts_rrc_types.SysInfoType15_4,
+                26: util.gsmtap_umts_rrc_types.SysInfoType18,
+                27: util.gsmtap_umts_rrc_types.SysInfoTypeSB1,
+                28: util.gsmtap_umts_rrc_types.SysInfoTypeSB2,
+                29: util.gsmtap_umts_rrc_types.SysInfoType15_5,
+                30: util.gsmtap_umts_rrc_types.SysInfoType5bis,
+                31: util.gsmtap_umts_rrc_types.SysInfoType19,
+                # Extension SIB
+                66: util.gsmtap_umts_rrc_types.SysInfoType11bis,
+                67: util.gsmtap_umts_rrc_types.SysInfoType19
+        }
+
+        if channel_type in channel_type_map.keys():
+            arfcn = self.umts_last_uarfcn_dl[radio_id]
+            if channel_type == 0 or channel_type == 1:
+                arfcn = self.umts_last_uarfcn_ul[radio_id]
+
+            subtype = channel_type_map[channel_type]
             msg_content = pkt[4:]
+        elif channel_type in channel_type_map_extended_type.keys():
+            arfcn = self.umts_last_uarfcn_dl[radio_id]
+
+            # uint8 subtype, uint8 msg[]
+            if pkt[4] in sib_type_map.keys():
+                subtype = sib_type_map[pkt[4]]
+                msg_content = pkt[5:]
+            else:
+                print("Unknown WCDMA SIB Class {}".format(pkt[4]))
+                return
+        elif channel_type in channel_type_map_new.keys():
+            # uint16 uarfcn, uint16 psc, uint8 msg[]
+            arfcn, psc = struct.unpack('<HH', pkt[4:8])
+
+            subtype = channel_type_map_new[channel_type]
+            msg_content = pkt[8:]
+        elif channel_type in channel_type_map_new_extended_type.keys():
+            # uint16 uarfcn, uint16 psc, uint8 subtype, uint8 msg[]
+            arfcn, psc = struct.unpack('<HH', pkt[4:8])
+
+            if pkt[8] in sib_type_map_new.keys():
+                subtype = sib_type_map_new[pkt[8]]
+                msg_content = pkt[9:]
+            else:
+                print("Unknown WCDMA new SIB Class {}".format(pkt[8]))
+                return
+        else:
+            print("Unknown WCDMA RRC channel type {}".format(pkt[0]))
+            util.xxd(pkt)
+            return
 
         ts_sec = calendar.timegm(pkt_ts.timetuple())
         ts_usec = pkt_ts.microsecond
@@ -705,10 +804,10 @@ class QualcommParser:
 
     def parse_umts_ue_ota_dsds(self, pkt_ts, pkt, radio_id):
         radio_id_pkt = pkt[0]
-        if radio_id > 2:
+        if radio_id_pkt < 1 or radio_id_pkt > 2:
             print('Unexpected radio ID {}'.format(radio_id_pkt))
             return
-        self.parse_umts_ue_ota(pkt_ts, pkt[1:], radio_id_pkt)
+        self.parse_umts_ue_ota(pkt_ts, pkt[1:], radio_id_pkt - 1)
 
     # LTE
 
@@ -1886,7 +1985,7 @@ class QualcommParser:
 
         xdm_hdr = pkt[0:8]
         xdm_hdr = struct.unpack('<BBHL', xdm_hdr) # cmd_id, unknown, dummy, subscription_id
-        if xdm_hdr[3] < 1:
+        if xdm_hdr[3] < 1 or xdm_hdr[3] > 2:
             print("Multi radio packet, unknown_1 = {}, subscription_id = {}".format(xdm_hdr[1], xdm_hdr[3]))
             util.xxd(pkt)
         pkt_body = pkt[8:]
