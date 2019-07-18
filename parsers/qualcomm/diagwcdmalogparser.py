715fc5365f30db493b6e280775aae984dfd51df1
Shinjo Park <peremen@gmail.com>
2019-07-18 19:08:26 +0200

    Parse 2G neighbor cell information in 3G cell search packets

--- parsers/qualcomm/diagwcdmalogparser.py
+++ parsers/qualcomm/diagwcdmalogparser.py
@@ -4,6 +4,7 @@ from . import diagcmd
 import util
 
 import struct
+from collections import namedtuple
 import calendar, datetime
 import logging
 
@@ -17,54 +18,110 @@ class DiagWcdmaLogParser:
             0x412F: lambda x, y, z: self.parse_wcdma_rrc(x, y, z), # WCDMA Signaling Messages
         }
 
+        self.WcdmaSearchCellReselectionV03G = namedtuple('WcdmaSearchCellReselectionV03G',
+            'uarfcn psc rscp rank_rscp ecio rank_ecio')
+        self.WcdmaSearchCellReselectionV13G = namedtuple('WcdmaSearchCellReselectionV13G',
+            'uarfcn psc rscp rank_rscp ecio rank_ecio resel_status')
+        self.WcdmaSearchCellReselectionV23G = namedtuple('WcdmaSearchCellReselectionV23G',
+            'uarfcn psc rscp rank_rscp ecio rank_ecio resel_status hcs_priority h_value hcs_cell_qualify')
+
+        self.WcdmaSearchCellReselectionV02G = namedtuple('WcdmaSearchCellReselectionV02G',
+            'arfcn bsic rssi rank')
+        self.WcdmaSearchCellReselectionV12G = namedtuple('WcdmaSearchCellReselectionV12G',
+            'arfcn bsic rssi rank resel_status')
+        self.WcdmaSearchCellReselectionV22G = namedtuple('WcdmaSearchCellReselectionV22G',
+            'arfcn bsic rssi rank resel_status hcs_priority h_value hcs_cell_qualify')
+
+    def get_real_rscp(self, rscp):
+        return rscp - 21
+
+    def get_real_ecio(self, ecio):
+        return ecio / 2
+
     # 3G
     def parse_wcdma_search_cell_reselection_v0(self, pkt_ts, pkt, radio_id):
         num_wcdma_cells = pkt[0] & 0x3f # lower 6b
-        num_gsm_cells = pkt[1] # TODO: check if num_gsm_cells > 0
+        num_gsm_cells = pkt[1] & 0x3f # lower 6b
 
-        print('Radio {}: 3G Cell: # cells {}'.format(self.parent.sanitize_radio_id(radio_id), num_wcdma_cells))
+        print('Radio {}: {} 3G cells, {} 2G cells'.format(self.parent.sanitize_radio_id(radio_id), num_wcdma_cells, num_gsm_cells))
+        cell_pkt_struct_3g = '<HHbhbh'
+        cell_pkt_size_3g = struct.calcsize(cell_pkt_struct_3g)
         for i in range(num_wcdma_cells):
-            cell_pkt = pkt[2 + 10 * i:2 + 10 * (i + 1)]
-            cell_pkt_vals = struct.unpack('<HHbhbh', cell_pkt)
-            n_cell_uarfcn = cell_pkt_vals[0]
-            n_cell_psc = cell_pkt_vals[1]
-            n_cell_rscp = cell_pkt_vals[2]
-            n_cell_rank_rscp = cell_pkt_vals[3]
-            n_cell_ecio = cell_pkt_vals[4]
-            n_cell_rank_ecio = cell_pkt_vals[5]
-            print('Radio {}: Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'.format(self.parent.sanitize_radio_id(radio_id), i, n_cell_uarfcn, n_cell_psc, n_cell_rscp - 21, n_cell_ecio / 2))
+            cell_pkt = pkt[2 + cell_pkt_size_3g * i: 2 + cell_pkt_size_3g * (i + 1)]
+            cell_pkt_vals = self.WcdmaSearchCellReselectionV03G._make(struct.unpack(cell_pkt_struct_3g, cell_pkt))
+            print('Radio {}: 3G Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'
+                .format(self.parent.sanitize_radio_id(radio_id), i, 
+                    cell_pkt_vals.uarfcn, cell_pkt_vals.psc, 
+                    self.get_real_rscp(cell_pkt_vals.rscp), self.get_real_ecio(cell_pkt_vals.ecio)))
+
+        if num_gsm_cells > 0:
+            gsm_cell_start_pos = 2 + cell_pkt_size_3g * num_wcdma_cells
+            cell_pkt_struct_2g = '<HHbhb'
+            cell_pkt_size_2g = struct.calcsize(cell_pkt_struct_2g)
+            for i in range(num_gsm_cells):
+                cell_pkt = pkt[gsm_cell_start_pos + cell_pkt_size_2g * i: gsm_cell_start_pos + cell_pkt_size_2g * (i + 1)]
+                cell_pkt_vals = self.WcdmaSearchCellReselectionV02G._make(struct.unpack(cell_pkt_struct_2g, cell_pkt))
+                print('Radio {}: 2G Cell {}: ARFCN {}, RSSI {:.2f}, Rank {}'
+                    .format(self.parent.sanitize_radio_id(radio_id), i, 
+                        cell_pkt_vals.arfcn & 0xfff,
+                        cell_pkt_vals.rssi,
+                        cell_pkt_vals.rank))
 
     def parse_wcdma_search_cell_reselection_v1(self, pkt_ts, pkt, radio_id):
         num_wcdma_cells = pkt[0] & 0x3f # lower 6b
-        num_gsm_cells = pkt[1] # TODO: check if num_gsm_cells > 0
+        num_gsm_cells = pkt[1] & 0x3f # lower 6b
 
-        print('Radio {}: 3G Cell: # cells {}'.format(self.parent.sanitize_radio_id(radio_id), num_wcdma_cells))
+        print('Radio {}: {} 3G cells, {} 2G cells'.format(self.parent.sanitize_radio_id(radio_id), num_wcdma_cells, num_gsm_cells))
+        cell_pkt_struct_3g = '<HHbhbhb'
+        cell_pkt_size_3g = struct.calcsize(cell_pkt_struct_3g)
         for i in range(num_wcdma_cells):
-            cell_pkt = pkt[2 + 11 * i:2 + 11 * (i + 1)]
-            cell_pkt_vals = struct.unpack('<HHbhbh', cell_pkt[:10])
-            n_cell_uarfcn = cell_pkt_vals[0]
-            n_cell_psc = cell_pkt_vals[1]
-            n_cell_rscp = cell_pkt_vals[2]
-            n_cell_rank_rscp = cell_pkt_vals[3]
-            n_cell_ecio = cell_pkt_vals[4]
-            n_cell_rank_ecio = cell_pkt_vals[5]
-            print('Radio {}: Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'.format(self.parent.sanitize_radio_id(radio_id), i, n_cell_uarfcn, n_cell_psc, n_cell_rscp - 21, n_cell_ecio / 2))
+            cell_pkt = pkt[2 + cell_pkt_size_3g * i: 2 + cell_pkt_size_3g * (i + 1)]
+            cell_pkt_vals = self.WcdmaSearchCellReselectionV13G._make(struct.unpack(cell_pkt_struct_3g, cell_pkt))
+            print('Radio {}: 3G Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'
+                .format(self.parent.sanitize_radio_id(radio_id), i, 
+                    cell_pkt_vals.uarfcn, cell_pkt_vals.psc, 
+                    self.get_real_rscp(cell_pkt_vals.rscp), self.get_real_ecio(cell_pkt_vals.ecio)))
+
+        if num_gsm_cells > 0:
+            gsm_cell_start_pos = 2 + cell_pkt_size_3g * num_wcdma_cells
+            cell_pkt_struct_2g = '<HHbhb'
+            cell_pkt_size_2g = struct.calcsize(cell_pkt_struct_2g)
+            for i in range(num_gsm_cells):
+                cell_pkt = pkt[gsm_cell_start_pos + cell_pkt_size_2g * i: gsm_cell_start_pos + cell_pkt_size_2g * (i + 1)]
+                cell_pkt_vals = self.WcdmaSearchCellReselectionV12G._make(struct.unpack(cell_pkt_struct_2g, cell_pkt))
+                print('Radio {}: 2G Cell {}: ARFCN {}, RSSI {:.2f}, Rank {}'
+                    .format(self.parent.sanitize_radio_id(radio_id), i, 
+                        cell_pkt_vals.arfcn & 0xfff,
+                        cell_pkt_vals.rssi,
+                        cell_pkt_vals.rank))
 
     def parse_wcdma_search_cell_reselection_v2(self, pkt_ts, pkt, radio_id):
         num_wcdma_cells = pkt[0] & 0x3f # lower 6b
-        num_gsm_cells = pkt[1] # TODO: check if num_gsm_cells > 0
+        num_gsm_cells = pkt[1] & 0x3f # lower 6b
 
-        print('Radio {}: 3G Cell: # cells {}'.format(self.parent.sanitize_radio_id(radio_id), num_wcdma_cells))
+        print('Radio {}: {} 3G cells, {} 2G cells'.format(self.parent.sanitize_radio_id(radio_id), num_wcdma_cells, num_gsm_cells))
+        cell_pkt_struct_3g = '<HHbhbhbhhb'
+        cell_pkt_size_3g = struct.calcsize(cell_pkt_struct_3g)
         for i in range(num_wcdma_cells):
-            cell_pkt = pkt[7 + 16 * i:7 + 16 * (i + 1)]
-            cell_pkt_vals = struct.unpack('<HHbhbh', cell_pkt[:10])
-            n_cell_uarfcn = cell_pkt_vals[0]
-            n_cell_psc = cell_pkt_vals[1]
-            n_cell_rscp = cell_pkt_vals[2]
-            n_cell_rank_rscp = cell_pkt_vals[3]
-            n_cell_ecio = cell_pkt_vals[4]
-            n_cell_rank_ecio = cell_pkt_vals[5]
-            print('Radio {}: Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'.format(self.parent.sanitize_radio_id(radio_id), i, n_cell_uarfcn, n_cell_psc, n_cell_rscp - 21, n_cell_ecio / 2))
+            cell_pkt = pkt[7 + cell_pkt_size_3g * i: 7 + cell_pkt_size_3g * (i + 1)]
+            cell_pkt_vals = self.WcdmaSearchCellReselectionV23G._make(struct.unpack(cell_pkt_struct_3g, cell_pkt))
+            print('Radio {}: 3G Cell {}: UARFCN {}, PSC {:3d}, RSCP {}, Ec/Io {:.2f}'
+                .format(self.parent.sanitize_radio_id(radio_id), i, 
+                    cell_pkt_vals.uarfcn, cell_pkt_vals.psc, 
+                    self.get_real_rscp(cell_pkt_vals.rscp), self.get_real_ecio(cell_pkt_vals.ecio)))
+
+        if num_gsm_cells > 0:
+            gsm_cell_start_pos = 7 + cell_pkt_size_3g * num_wcdma_cells
+            cell_pkt_struct_2g = '<HHbhbhhb'
+            cell_pkt_size_2g = struct.calcsize(cell_pkt_struct_2g)
+            for i in range(num_gsm_cells):
+                cell_pkt = pkt[gsm_cell_start_pos + cell_pkt_size_2g * i: gsm_cell_start_pos + cell_pkt_size_2g * (i + 1)]
+                cell_pkt_vals = self.WcdmaSearchCellReselectionV22G._make(struct.unpack(cell_pkt_struct_2g, cell_pkt))
+                print('Radio {}: 2G Cell {}: ARFCN {}, RSSI {:.2f}, Rank {}'
+                    .format(self.parent.sanitize_radio_id(radio_id), i, 
+                        cell_pkt_vals.arfcn & 0xfff,
+                        cell_pkt_vals.rssi,
+                        cell_pkt_vals.rank))
 
     def parse_wcdma_search_cell_reselection(self, pkt_ts, pkt, radio_id):
         pkt_version = (pkt[0] >> 6) # upper 2b
