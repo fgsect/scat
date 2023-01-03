679a86bef147094f64d6693e5aff9ca1db29f920
Shinjo Park <peremen@gmail.com>
2023-01-03 11:08:18 +0100

    sdmipparser: fix IP packet parsing

--- parsers/samsung/sdmipparser.py
+++ parsers/samsung/sdmipparser.py
@@ -5,6 +5,8 @@ import util
 
 import struct
 import logging
+import binascii
+from collections import namedtuple
 
 class SdmIpParser:
     def __init__(self, parent):
@@ -12,23 +14,26 @@ class SdmIpParser:
 
         self.process = {
             (sdm_command_group.CMD_IP_DATA << 8) | 0x00: lambda x: self.sdm_ip_data(x),
+            (sdm_command_group.CMD_IP_DATA << 8) | 0x10: lambda x: self.sdm_0x0710(x),
         }
 
     def sdm_ip_data(self, pkt):
-        pkt = pkt[11:-1]
+        # Unknown: 0x0800, 0x150D
+        pkt = pkt[15:-1]
 
-        ip_hdr = struct.unpack('<BLHHHH', pkt[0:13])
-        # 00 ts(uint32) stamp(uint16) dir(uint16) ?(uint16) len(uint16)
-        # 0: Data type (0x00: IP, 0x10: Unknown)
-        # 1: TS
-        # 2: Packet #
-        # 3: Direction
-        # 4: Unknown
-        # 5: Length
-        ip_payload = pkt[13:]
+        header_struct = namedtuple('SdmIpData', 'seq_num direction unknown length')
+        header = header_struct._make(struct.unpack('<HHHH', pkt[0:8]))
+        payload = pkt[8:]
 
-        if ip_hdr[0] == 0x00:
-            if ip_hdr[5] != len(ip_payload):
-                if self.parent:
-                    self.parent.logger.log(logging.WARNING, 'IP length mismatch, expected 0x{:04x}, got 0x{:04x}'.format(ip_hdr[5], len(ip_payload)))
-            return {'up': [ip_payload]}
+        if header.length != len(payload):
+            if self.parent:
+                self.parent.logger.log(logging.WARNING, 'IP length mismatch, expected 0x{:04x}, got 0x{:04x}'.format(header.length, len(payload)))
+        else:
+            return {'up': [payload]}
+
+    def sdm_0x0710(self, pkt):
+        pkt = pkt[15:-1]
+        header_struct = namedtuple('Sdm0x0710Data', 'seq_num direction')
+        header = header_struct._make(struct.unpack('<HH', pkt[0:4]))
+        payload = pkt[4:]
+        return {'stdout': 'SDM 0x0710: {}, {}'.format(header, binascii.hexlify(payload).decode('utf-8'))}
