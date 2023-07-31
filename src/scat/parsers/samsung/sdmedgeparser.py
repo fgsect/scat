f6f00ae9037eee352e99b1e414383c49a8320e1d
Shinjo Park <peremen@gmail.com>
2023-07-31 22:46:11 +0200

    sdmedgeparser: add measurement information

--- src/scat/parsers/samsung/sdmedgeparser.py
+++ src/scat/parsers/samsung/sdmedgeparser.py
@@ -22,7 +22,7 @@ class SdmEdgeParser:
             (sdm_command_group.CMD_EDGE_DATA << 8) | sdm_edge_data.EDGE_3G_NCELL_INFO: lambda x: self.sdm_edge_dummy(x, 0x07),
             (sdm_command_group.CMD_EDGE_DATA << 8) | sdm_edge_data.EDGE_HANDOVER_INFO: lambda x: self.sdm_edge_dummy(x, 0x08),
             (sdm_command_group.CMD_EDGE_DATA << 8) | sdm_edge_data.EDGE_HANDOVER_HISTORY_INFO: lambda x: self.sdm_edge_dummy(x, 0x09),
-            (sdm_command_group.CMD_EDGE_DATA << 8) | sdm_edge_data.EDGE_MEAS_INFO: lambda x: self.sdm_edge_dummy(x, 0x0b),
+            (sdm_command_group.CMD_EDGE_DATA << 8) | sdm_edge_data.EDGE_MEAS_INFO: lambda x: self.sdm_edge_meas_info(x),
         }
 
     def set_model(self, model):
@@ -44,8 +44,12 @@ class SdmEdgeParser:
         lac = struct.unpack('>H', scell_info.lai[3:5])[0]
         cid = struct.unpack('>H', struct.pack('<H',scell_info.cid))[0]
 
-        stdout = 'EDGE Serving Cell Info: ARFCN: {}, BSIC: {:#x}, RxLev: {}, PLMN: MCC {:x}/MNC {:x}, LAC: {:#x}, RAC: {:#x}, CID: {:#x}\n'.format(
-            scell_info.arfcn, scell_info.bsic, scell_info.rxlev - 110, plmn_str[0], plmn_str[1], lac, scell_info.rac, cid
+        if scell_info.arfcn > 1024:
+            # Invalid measurement
+            return {'stdout': ''}
+
+        stdout = 'EDGE Serving Cell Info: ARFCN: {}, BSIC: {:#x}, RxLev: {} (RSSI: {}), PLMN: MCC {:x}/MNC {:x}, LAC: {:#x}, RAC: {:#x}, CID: {:#x}\n'.format(
+            scell_info.arfcn, scell_info.bsic, scell_info.rxlev, scell_info.rxlev - 110, plmn_str[0], plmn_str[1], lac, scell_info.rac, cid
         )
 
         return {'stdout': stdout.rstrip()}
@@ -74,4 +78,26 @@ class SdmEdgeParser:
         return {'stdout': ''}
 
     def sdm_edge_meas_info(self, pkt):
-        return {'stdout': ''}
+        sdm_pkt_hdr = parse_sdm_header(pkt[1:15])
+        pkt = pkt[15:-1]
+        header_s = namedtuple('SdmEdgeSCellMeasInfo', 'arfcn bsic rxlev rxlev_p rxq_p rxlev_s rxq_s txlev')
+        header_n = namedtuple('SdmEdgeNCellMeasInfo', 'arfcn bsic rxlev unk')
+
+        scell_meas_info = header_s._make(struct.unpack('<HHHHHHHH', pkt[0:16]))
+        extra = pkt[16:]
+
+        if scell_meas_info.arfcn < 1024:
+            stdout = 'EDGE Measurement Info (Serving Cell): ARFCN {}, BSIC {:#04x}, RxLev {} (RSSI {}), TxLev {}\n'.format(
+                scell_meas_info.arfcn, scell_meas_info.bsic,
+                scell_meas_info.rxlev, scell_meas_info.rxlev - 110, scell_meas_info.txlev)
+        else:
+            stdout = ''
+
+        for i in range(int(len(extra)/10)):
+            ncell_meas_info = header_n._make(struct.unpack('<HHHL', extra[i*10:(i+1)*10]))
+            if ncell_meas_info.arfcn < 1024:
+                stdout += 'EDGE Measurement Info (Neighbor Cell): ARFCN {}, BSIC {:#04x}, RxLev {} (RSSI {})\n'.format(
+                    ncell_meas_info.arfcn, ncell_meas_info.bsic & 0b111111,
+                    ncell_meas_info.rxlev, ncell_meas_info.rxlev - 110)
+
+        return {'stdout': stdout.rstrip()}
--- tests/test_sdmedgeparser.py
+++ tests/test_sdmedgeparser.py
@@ -14,25 +14,25 @@ class TestSdmEdgeParser(unittest.TestCase):
         payload = binascii.unhexlify('ffff00000000000000000000000000000000000000000000000000000000000000000000ffff')
         packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_SCELL_INFO, payload, timestamp=0x0)
         result = self.parser.sdm_edge_scell_info(packet)
-        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 65535, BSIC: 0x0, RxLev: -110, PLMN: MCC 0/MNC 0, LAC: 0x0, RAC: 0x0, CID: 0x0'''}
+        expected = {'stdout': ''}
         self.assertDictEqual(result, expected)
 
         payload = binascii.unhexlify('2c003d2200080162f2200134012e060001000101000000000000000021011c1cffffffffc202')
         packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_SCELL_INFO, payload, timestamp=0x0)
         result = self.parser.sdm_edge_scell_info(packet)
-        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 44, BSIC: 0x3d, RxLev: -76, PLMN: MCC 262/MNC 2, LAC: 0x134, RAC: 0x1, CID: 0x2e06'''}
+        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 44, BSIC: 0x3d, RxLev: 34 (RSSI: -76), PLMN: MCC 262/MNC 2, LAC: 0x134, RAC: 0x1, CID: 0x2e06'''}
         self.assertDictEqual(result, expected)
 
         payload = binascii.unhexlify('04003f1e00060162f220014101291b0001000101000000000000000021021a1affffffffc202')
         packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_SCELL_INFO, payload, timestamp=0x0)
         result = self.parser.sdm_edge_scell_info(packet)
-        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 4, BSIC: 0x3f, RxLev: -80, PLMN: MCC 262/MNC 2, LAC: 0x141, RAC: 0x1, CID: 0x291b'''}
+        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 4, BSIC: 0x3f, RxLev: 30 (RSSI: -80), PLMN: MCC 262/MNC 2, LAC: 0x141, RAC: 0x1, CID: 0x291b'''}
         self.assertDictEqual(result, expected)
 
         payload = binascii.unhexlify('3500141c00060062f210140701bb4400010001000000000000000000210018f9ffffffffd601')
         packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_SCELL_INFO, payload, timestamp=0x0)
         result = self.parser.sdm_edge_scell_info(packet)
-        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 53, BSIC: 0x14, RxLev: -82, PLMN: MCC 262/MNC 1, LAC: 0x1407, RAC: 0x1, CID: 0xbb44'''}
+        expected = {'stdout': '''EDGE Serving Cell Info: ARFCN: 53, BSIC: 0x14, RxLev: 28 (RSSI: -82), PLMN: MCC 262/MNC 1, LAC: 0x1407, RAC: 0x1, CID: 0xbb44'''}
         self.assertDictEqual(result, expected)
 
     def test_sdm_edge_ncell_info(self):
@@ -88,8 +88,44 @@ class TestSdmEdgeParser(unittest.TestCase):
         self.assertDictEqual(result, expected)
 
     def test_sdm_edge_meas_info(self):
-        # 4400 32 00 11 00 0f 0000 000f00 00 0000 003a002d00020021000000340020000900000000003f00ff000100925302000b00ff0001007e4329004e00ff000800010000004500ff0002007c0027004300ff0001006f1c27004d003a000100551f0000ffffff001d00d3470d00
-        pass
+        payload = binascii.unhexlify('4400320011000f0000000f00000000003a002d00020021000000340020000900000000003f00ff000100925302000b00ff0001007e4329004e00ff000800010000004500ff0002007c0027004300ff0001006f1c27004d003a000100551f0000ffffff001d00d3470d00')
+        packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_MEAS_INFO, payload, timestamp=0x0)
+        result = self.parser.sdm_edge_meas_info(packet)
+        expected = {'stdout': '''EDGE Measurement Info (Serving Cell): ARFCN 68, BSIC 0x32, RxLev 17 (RSSI -93), TxLev 0
+EDGE Measurement Info (Neighbor Cell): ARFCN 58, BSIC 0x2d, RxLev 2 (RSSI -108)
+EDGE Measurement Info (Neighbor Cell): ARFCN 52, BSIC 0x20, RxLev 9 (RSSI -101)
+EDGE Measurement Info (Neighbor Cell): ARFCN 63, BSIC 0x3f, RxLev 1 (RSSI -109)
+EDGE Measurement Info (Neighbor Cell): ARFCN 11, BSIC 0x3f, RxLev 1 (RSSI -109)
+EDGE Measurement Info (Neighbor Cell): ARFCN 78, BSIC 0x3f, RxLev 8 (RSSI -102)
+EDGE Measurement Info (Neighbor Cell): ARFCN 69, BSIC 0x3f, RxLev 2 (RSSI -108)
+EDGE Measurement Info (Neighbor Cell): ARFCN 67, BSIC 0x3f, RxLev 1 (RSSI -109)
+EDGE Measurement Info (Neighbor Cell): ARFCN 77, BSIC 0x3a, RxLev 1 (RSSI -109)'''}
+        self.assertDictEqual(result, expected)
+
+        payload = binascii.unhexlify('350014002100000000000000000000003b0018001a00756407004b00ff001300756407003900ff001400000000003e00ff000c00571108003300ff000e00756407004200ff000e000100000035001400220075640700ffffff00000000000000ffffff00000000000000')
+        packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_MEAS_INFO, payload, timestamp=0x0)
+        result = self.parser.sdm_edge_meas_info(packet)
+        expected = {'stdout': '''EDGE Measurement Info (Serving Cell): ARFCN 53, BSIC 0x14, RxLev 33 (RSSI -77), TxLev 0
+EDGE Measurement Info (Neighbor Cell): ARFCN 59, BSIC 0x18, RxLev 26 (RSSI -84)
+EDGE Measurement Info (Neighbor Cell): ARFCN 75, BSIC 0x3f, RxLev 19 (RSSI -91)
+EDGE Measurement Info (Neighbor Cell): ARFCN 57, BSIC 0x3f, RxLev 20 (RSSI -90)
+EDGE Measurement Info (Neighbor Cell): ARFCN 62, BSIC 0x3f, RxLev 12 (RSSI -98)
+EDGE Measurement Info (Neighbor Cell): ARFCN 51, BSIC 0x3f, RxLev 14 (RSSI -96)
+EDGE Measurement Info (Neighbor Cell): ARFCN 66, BSIC 0x3f, RxLev 14 (RSSI -96)
+EDGE Measurement Info (Neighbor Cell): ARFCN 53, BSIC 0x14, RxLev 34 (RSSI -76)'''}
+        self.assertDictEqual(result, expected)
+
+        payload = binascii.unhexlify('350014001e00000000000000000021003b001800120000000000390004000a00e3ac00004b00ff00110047a100004900ff000d0047a100003e0005000d002c57260042000b000a00bd0b1000ffffff00000000000000ffffff00000000000000ffffff00000000000000')
+        packet = sdmcmd.generate_sdm_packet(0xa0, sdmcmd.sdm_command_group.CMD_EDGE_DATA, sdmcmd.sdm_edge_data.EDGE_MEAS_INFO, payload, timestamp=0x0)
+        result = self.parser.sdm_edge_meas_info(packet)
+        expected = {'stdout': '''EDGE Measurement Info (Serving Cell): ARFCN 53, BSIC 0x14, RxLev 30 (RSSI -80), TxLev 33
+EDGE Measurement Info (Neighbor Cell): ARFCN 59, BSIC 0x18, RxLev 18 (RSSI -92)
+EDGE Measurement Info (Neighbor Cell): ARFCN 57, BSIC 0x04, RxLev 10 (RSSI -100)
+EDGE Measurement Info (Neighbor Cell): ARFCN 75, BSIC 0x3f, RxLev 17 (RSSI -93)
+EDGE Measurement Info (Neighbor Cell): ARFCN 73, BSIC 0x3f, RxLev 13 (RSSI -97)
+EDGE Measurement Info (Neighbor Cell): ARFCN 62, BSIC 0x05, RxLev 13 (RSSI -97)
+EDGE Measurement Info (Neighbor Cell): ARFCN 66, BSIC 0x0b, RxLev 10 (RSSI -100)'''}
+        self.assertDictEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
