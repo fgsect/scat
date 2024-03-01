41f6dadde9d7f6bc3e790a15dc5edbc2d3c16a8b
peacey <peaceyall@gmail.com>
2024-02-29 19:27:54 -0700

    Add new versions to diagnrlogparser tests

--- tests/test_diagnrlogparser.py
+++ tests/test_diagnrlogparser.py
@@ -65,12 +65,31 @@ class TestDiagNrLogParser(unittest.TestCase):
             'ts': datetime.datetime(1980, 1, 6, 0, 0, tzinfo=datetime.timezone.utc)}
         self.assertDictEqual(result, expected)
 
+        # Version 0x30003
+        payload = binascii.unhexlify('030003000101004b0001c83b57252230001aee0100080e02000a000a0001c83b57050000002e0103dc00008eb921004700')
+        pkt_header = self.log_header(cmd_code=0x10, reserved=0, length1=len(payload) + 12, length2=len(payload) + 12,
+                                     log_id=diagcmd.diag_log_get_lte_item_id(diagcmd.diag_log_code_5gnr.LOG_5GNR_RRC_SERVING_CELL_INFO), timestamp=0)
+        result = self.parser.parse_nr_rrc_scell_info(pkt_header, payload, None)
+        expected = {'stdout': 'NR RRC SCell Info: NR-ARFCN 126490/134664, Bandwidth 10/10 MHz, Band 71, PCI   75, xTAC/xCID 21b98e/5573bc801, MCC 302, MNC 220',
+            'ts': datetime.datetime(1980, 1, 6, 0, 0, tzinfo=datetime.timezone.utc)}
+        self.assertDictEqual(result, expected)
+
+
     def test_parse_nr_mm_state(self):
         payload = binascii.unhexlify('0100000003000054f0800254f080a206001636ac480400a040fe')
         pkt_header = self.log_header(cmd_code=0x10, reserved=0, length1=len(payload) + 12, length2=len(payload) + 12,
                                      log_id=diagcmd.diag_log_get_lte_item_id(diagcmd.diag_log_code_5gnr.LOG_5GNR_NAS_5GMM_STATE), timestamp=0)
         result = self.parser.parse_nr_mm_state(pkt_header, payload, None)
         expected = {'stdout': '5GMM State: 3/0/0, PLMN: 450/  8, TAC: a040fe, GUTI: 450-008-a2-006-16-0448ac36',
+            'ts': datetime.datetime(1980, 1, 6, 0, 0, tzinfo=datetime.timezone.utc)}
+        self.assertDictEqual(result, expected)
+
+        # Version 0x30000
+        payload = binascii.unhexlify('000003000300000302220203022255c40332d6c214c00021b98e00')
+        pkt_header = self.log_header(cmd_code=0x10, reserved=0, length1=len(payload) + 12, length2=len(payload) + 12,
+                                     log_id=diagcmd.diag_log_get_lte_item_id(diagcmd.diag_log_code_5gnr.LOG_5GNR_NAS_5GMM_STATE), timestamp=0)
+        result = self.parser.parse_nr_mm_state(pkt_header, payload, None)
+        expected = {'stdout': '5GMM State: 3/0/0, PLMN: 302/220, TAC: 21b98e, GUTI: 302-220-55-3c4-32-c014c2d6',
             'ts': datetime.datetime(1980, 1, 6, 0, 0, tzinfo=datetime.timezone.utc)}
         self.assertDictEqual(result, expected)
 
