5e6257c88cc504514238a3f9fe89e7a1cfd518d0
Shinjo Park <peremen@gmail.com>
2025-03-23 22:25:03 +0100

    diag1xlogparser: reordering

--- src/scat/parsers/qualcomm/diag1xlogparser.py
+++ src/scat/parsers/qualcomm/diag1xlogparser.py
@@ -43,8 +43,11 @@ class Diag1xLogParser:
             #0x1098: lambda x, y, z: self.parse_sim(x, y, z, 0), # RUIM Debug
             #0x14CE: lambda x, y, z: self.parse_sim(x, y, z, 1), # UIM DS Data
 
-            # Generic
+            # IP
             i(x.LOG_DATA_PROTOCOL_LOGGING_C): lambda x, y, z: self.parse_ip(x, y, z), # Protocol Services Data
+
+            # IMS
+            i(x.LOG_IMS_SIP_MESSAGE): lambda x, y, z: self.parse_sip_message(x, y, z),
 
             # QMI
             # i(x.LOG_QMI_LINK_01_RX_MSG_C): lambda x, y, z: self.parse_qmi_message(x, y, z, 1, False),
@@ -91,91 +94,18 @@ class Diag1xLogParser:
             # i(x.LOG_QMI_LINK_21_TX_MSG_C): lambda x, y, z: self.parse_qmi_message(x, y, z, 21, True),
             i(x.LOG_QMI_CALL_FLOW_C): lambda x, y, z: self.parse_qmi_call_flow(x, y, z),
             i(x.LOG_QMI_SUPPORTED_INTERFACES_C): lambda x, y, z: self.parse_qmi_supported_interfaces(x, y, z),
-
-            # IMS
-            i(x.LOG_IMS_SIP_MESSAGE): lambda x, y, z: self.parse_sip_message(x, y, z),
         }
 
     def update_parameters(self, display_format, gsmtapv3):
         self.display_format = display_format
         self.gsmtapv3 = gsmtapv3
 
-    def parse_ip(self, pkt_header, pkt_body, args):
-        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
-        item_struct = namedtuple('QcDiag1xProtocolData', 'instance protocol ifnameid direction sequence_num segment_num_is_final')
-        item = item_struct._make(struct.unpack('<BBBBHH', pkt_body[0:8]))
-        item_data = pkt_body[8:]
+    def parse_1x_stub(self, pkt_ts, pkt, radio_id, item_id):
+        if self.parent:
+            self.parent.logger.log(logging.WARNING, "DIAG_1x_STUB: {:#x}".format(item_id))
+            self.parent.logger.log(logging.DEBUG, "Body: {}".format(util.xxd_oneline(pkt).decode()))
 
-        # pkt[3] = 0a00 0000 [a: direction, 0=RX, 1=TX]
-        is_tx = True if (item.direction & 0x40 == 0x40) else False
-        segment_num = item.segment_num_is_final & 0x7fff
-        # pkt[5]: segn/fin_seg (0x8000: fin_seg, 0x7fff: segn)
-        is_final_segment = True if (item.segment_num_is_final & 0x8000 == 0x8000) else False
-        pkt_buf = b''
-        pkt_id = (item.ifnameid, is_tx, item.sequence_num)
-
-        if item.protocol != 0x01:
-            if self.parent:
-                self.parent.logger.log(logging.WARNING, "Data type {} is not IP".format(item.protocol))
-
-        if is_final_segment:
-            if segment_num == 0:
-                return {'up': [item_data], 'ts': pkt_ts}
-            else:
-                if not (pkt_id in self.pending_pkts.keys()):
-                    return {'up': [item_data], 'ts': pkt_ts}
-                pending_pkt = self.pending_pkts.get(pkt_id)
-                for x in range(segment_num):
-                    if not (x in pending_pkt.keys()):
-                        if self.parent:
-                            self.parent.logger.log(logging.WARNING, "Segment {} for data packet ({}, {}, {}) missing".format(x, item.ifnameid, is_tx, item.sequence_num))
-                        continue
-                    pkt_buf += pending_pkt[x]
-                del self.pending_pkts[pkt_id]
-                pkt_buf += item_data
-                return {'up': [pkt_buf], 'ts': pkt_ts}
-        else:
-            if pkt_id in self.pending_pkts.keys():
-                self.pending_pkts[pkt_id][segment_num] = item_data
-            else:
-                self.pending_pkts[pkt_id] = {segment_num: item_data}
-
-    def parse_qmi_message(self, pkt_header, pkt_body, args, qmi_port, is_tx):
-        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
-        ts_sec = calendar.timegm(pkt_ts.timetuple())
-        ts_usec = pkt_ts.microsecond
-        stdout = ''
-
-        if not has_gobject:
-            if self.parent:
-                self.parent.logger.log(logging.ERROR, 'Decoding QMI message requires PyGObject and libqmi-glib package')
-                return
-
-        stdout += 'QMI Port: {}, Direction: {}, Raw data: {}'.format(qmi_port, 'TX' if is_tx else 'RX', binascii.hexlify(pkt_body).decode())
-        try:
-            q = Qmi.message_new_from_raw(pkt_body)
-            stdout += ', {}'.format(str(q))
-        except gi.repository.GLib.GError as e:
-            stdout += ', error: {}'.format(e)
-
-        return {'stdout': stdout, 'ts': pkt_ts}
-
-    def parse_qmi_call_flow(self, pkt_header, pkt_body, args):
-        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
-        ts_sec = calendar.timegm(pkt_ts.timetuple())
-        ts_usec = pkt_ts.microsecond
-
-        stdout = 'QMI_CALL_FLOW: {}'.format(binascii.hexlify(pkt_body).decode())
-        return {'stdout': stdout, 'ts': pkt_ts}
-
-    def parse_qmi_supported_interfaces(self, pkt_header, pkt_body, args):
-        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
-        ts_sec = calendar.timegm(pkt_ts.timetuple())
-        ts_usec = pkt_ts.microsecond
-
-        stdout = 'QMI_IFACES: {}'.format(binascii.hexlify(pkt_body).decode())
-        return {'stdout': stdout, 'ts': pkt_ts}
-
+    # SIM
     def parse_sim(self, pkt_header, pkt_body, args, sim_id):
         pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
         ts_sec = calendar.timegm(pkt_ts.timetuple())
@@ -223,6 +153,48 @@ class Diag1xLogParser:
                 self.last_tx[sim_id] = b''
                 return {'cp': [gsmtap_hdr + last_sim_tx, gsmtap_hdr + tx_buf], 'ts': pkt_ts}
 
+    # IP
+    def parse_ip(self, pkt_header, pkt_body, args):
+        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
+        item_struct = namedtuple('QcDiag1xProtocolData', 'instance protocol ifnameid direction sequence_num segment_num_is_final')
+        item = item_struct._make(struct.unpack('<BBBBHH', pkt_body[0:8]))
+        item_data = pkt_body[8:]
+
+        # pkt[3] = 0a00 0000 [a: direction, 0=RX, 1=TX]
+        is_tx = True if (item.direction & 0x40 == 0x40) else False
+        segment_num = item.segment_num_is_final & 0x7fff
+        # pkt[5]: segn/fin_seg (0x8000: fin_seg, 0x7fff: segn)
+        is_final_segment = True if (item.segment_num_is_final & 0x8000 == 0x8000) else False
+        pkt_buf = b''
+        pkt_id = (item.ifnameid, is_tx, item.sequence_num)
+
+        if item.protocol != 0x01:
+            if self.parent:
+                self.parent.logger.log(logging.WARNING, "Data type {} is not IP".format(item.protocol))
+
+        if is_final_segment:
+            if segment_num == 0:
+                return {'up': [item_data], 'ts': pkt_ts}
+            else:
+                if not (pkt_id in self.pending_pkts.keys()):
+                    return {'up': [item_data], 'ts': pkt_ts}
+                pending_pkt = self.pending_pkts.get(pkt_id)
+                for x in range(segment_num):
+                    if not (x in pending_pkt.keys()):
+                        if self.parent:
+                            self.parent.logger.log(logging.WARNING, "Segment {} for data packet ({}, {}, {}) missing".format(x, item.ifnameid, is_tx, item.sequence_num))
+                        continue
+                    pkt_buf += pending_pkt[x]
+                del self.pending_pkts[pkt_id]
+                pkt_buf += item_data
+                return {'up': [pkt_buf], 'ts': pkt_ts}
+        else:
+            if pkt_id in self.pending_pkts.keys():
+                self.pending_pkts[pkt_id][segment_num] = item_data
+            else:
+                self.pending_pkts[pkt_id] = {segment_num: item_data}
+
+    # IMS
     def parse_sip_message(self, pkt_header, pkt_body, args):
         pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
         item_struct = namedtuple('QcDiag1xSipMessage', 'version direction has_sdp len_call_id len_pkt len_pkt_real msg_type status_code unk4')
@@ -250,3 +222,40 @@ class Diag1xLogParser:
         self.ip_id += 1
 
         return {'up': [ip_hdr+udp_hdr+sip_body], 'ts': pkt_ts}
+
+    # QMI
+    def parse_qmi_message(self, pkt_header, pkt_body, args, qmi_port, is_tx):
+        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
+        ts_sec = calendar.timegm(pkt_ts.timetuple())
+        ts_usec = pkt_ts.microsecond
+        stdout = ''
+
+        if not has_gobject:
+            if self.parent:
+                self.parent.logger.log(logging.ERROR, 'Decoding QMI message requires PyGObject and libqmi-glib package')
+                return
+
+        stdout += 'QMI Port: {}, Direction: {}, Raw data: {}'.format(qmi_port, 'TX' if is_tx else 'RX', binascii.hexlify(pkt_body).decode())
+        try:
+            q = Qmi.message_new_from_raw(pkt_body)
+            stdout += ', {}'.format(str(q))
+        except gi.repository.GLib.GError as e:
+            stdout += ', error: {}'.format(e)
+
+        return {'stdout': stdout, 'ts': pkt_ts}
+
+    def parse_qmi_call_flow(self, pkt_header, pkt_body, args):
+        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
+        ts_sec = calendar.timegm(pkt_ts.timetuple())
+        ts_usec = pkt_ts.microsecond
+
+        stdout = 'QMI_CALL_FLOW: {}'.format(binascii.hexlify(pkt_body).decode())
+        return {'stdout': stdout, 'ts': pkt_ts}
+
+    def parse_qmi_supported_interfaces(self, pkt_header, pkt_body, args):
+        pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
+        ts_sec = calendar.timegm(pkt_ts.timetuple())
+        ts_usec = pkt_ts.microsecond
+
+        stdout = 'QMI_IFACES: {}'.format(binascii.hexlify(pkt_body).decode())
+        return {'stdout': stdout, 'ts': pkt_ts}
