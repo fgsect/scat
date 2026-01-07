bb4022c3d353975004fcbcd2c7d86b62f4c41020
Maxim Dernovoi <maxim.dernovoi@hochschule-stralsund.de>
2026-01-07 17:56:56 +0100

    pcapngwriter: Implement a PcapNG writer.

--- src/scat/writers/__init__.py
+++ src/scat/writers/__init__.py
@@ -3,5 +3,6 @@
 
 from scat.writers.abstractwriter import AbstractWriter
 from scat.writers.pcapwriter import PcapWriter
+from scat.writers.pcapngwriter import PcapngWriter
 from scat.writers.socketwriter import SocketWriter
 from scat.writers.rawwriter import RawWriter
---
+++ src/scat/writers/pcapngwriter.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+import struct
+import datetime
+
+from scat.writers.abstractwriter import AbstractWriter
+
+class PcapngWriter(AbstractWriter):
+    def __init__(self, filename: str, port_cp: int = 4729, port_up: int = 47290):
+        self.port_cp = port_cp
+        self.port_up = port_up
+        self.ip_id = 0
+        self.base_address = 0x7f000001
+        self.pcapng_file = open(filename, 'wb')
+        self.eth_hdr = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08\x00'
+
+        # Section Header Block (SHB)
+        section_header_block_content = struct.pack('<LHHQ',
+                0x1a2b3c4d,           # Byte-order magic
+                0x0001,                   # Major version
+                0x0000,                   # Minor version
+                0xffffffffffffffff,       # Section length (size unknown)
+        )
+        section_header_block = self._create_pcapng_block(0x0a0d0d0a, section_header_block_content)
+
+        # Interface Description Block (IDB)
+        interface_description_block_content = struct.pack('<HxxL',
+                0x0001,         # Link type (Ethernet)
+                0x00000000,         # Snap length (no limit)
+                )
+        interface_description_block_block = self._create_pcapng_block(0x00000001, interface_description_block_content)
+
+        self.pcapng_file.write(section_header_block)
+        self.pcapng_file.write(interface_description_block_block)
+
+    def __enter__(self):
+        return self
+
+    def write_pkt(self, sock_content: bytes, port: int, radio_id: int=0, ts: datetime.datetime = datetime.datetime.now(), options: list[bytes] = None) -> None:
+
+        if radio_id <= 0:
+            dest_address = self.base_address
+        else:
+            dest_address = self.base_address + radio_id
+
+        ip_hdr = struct.pack('!BBHHBBBBHLL',
+                             0x45,  # version, IHL, dsf
+                             0x00,
+                             len(sock_content) + 8 + 20,  # length
+                             self.ip_id,  # id
+                             0x40,  # flags/fragment offset
+                             0x00,
+                             0x40,  # TTL
+                             0x11,  # proto = udp
+                             0xffff,  # header checksum
+                             0x7f000001,  # src address
+                             dest_address,  # dest address
+                             )
+        udp_hdr = struct.pack('!HHHH',
+                              13337,  # source port
+                              port,  # destination port
+                              len(sock_content) + 8,  # length
+                              0xffff,  # checksum
+                              )
+
+        packet_data = self.eth_hdr + ip_hdr + udp_hdr + sock_content
+
+        timestamp_us = int(ts.timestamp() * 1000000)
+        timestamp_upper = (timestamp_us >> 32) & 0xFFFFFFFF
+        timestamp_lower = timestamp_us & 0xFFFFFFFF
+
+        enhanced_packet_block_header = struct.pack('<LLLLL',
+                                                   0,             # Interface ID
+                                                   timestamp_upper,   # Timestamp (high)
+                                                   timestamp_lower,   # Timestamp (low)
+                                                   len(packet_data),  # Captured packet length
+                                                   len(packet_data),  # Original packet length
+                                                   )
+
+        options_data = b''
+        if options is not None and len(options) > 0:
+            padding_len = (4 - (len(packet_data) % 4)) % 4  # outer %4 for case where len(packet_data) % 4 == 0
+            packet_data = packet_data + b'\x00' * padding_len
+
+            options_data = b''.join(options)
+            options_data += struct.pack('<HH',
+                                        0,  # 00 00 = opt_endofopt
+                                        0       # 00 00 = opt_endofopt length (must be 0)
+                                        )
+
+        enhanced_packet_block_content = enhanced_packet_block_header + packet_data + options_data
+        enhanced_packet_block = self._create_pcapng_block(0x00000006, enhanced_packet_block_content)
+        self.pcapng_file.write(enhanced_packet_block)
+
+        self.ip_id += 1
+        if self.ip_id > 65535:
+            self.ip_id = 0
+
+    def write_cp(self, sock_content: bytes, radio_id: int=0, ts: datetime.datetime=datetime.datetime.now()):
+        self.write_pkt(sock_content, self.port_cp, radio_id, ts)
+
+    def write_up(self, sock_content: bytes, radio_id: int=0, ts: datetime.datetime=datetime.datetime.now()):
+        self.write_pkt(sock_content, self.port_up, radio_id, ts)
+
+    def write_cp_with_options(self, sock_content: bytes, radio_id: int=0, ts: datetime.datetime=datetime.datetime.now(), options: list[bytes] = None):
+        self.write_pkt(sock_content, self.port_cp, radio_id, ts, options)
+
+    def write_up_with_options(self, sock_content: bytes, radio_id: int=0, ts: datetime.datetime=datetime.datetime.now(), options: list[bytes] = None):
+        self.write_pkt(sock_content, self.port_up, radio_id, ts, options)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.pcapng_file.close()
+
+    @classmethod
+    def create_comment_option(cls, comment: str) -> bytes:
+
+        comment_bytes = comment.encode('utf-8')
+
+        option = struct.pack('<HH',
+                             1,                 # option type (1 for comment)
+                             len(comment_bytes)     # option length
+                             )
+        option += comment_bytes
+
+        padding_len = (4 - (len(option) % 4)) % 4  # outer %4 for case where len(option) % 4 == 0
+        option += b'\x00' * padding_len
+
+        return option
+
+    def _create_pcapng_block(self, block_type: int, content: bytes) -> bytes:
+
+        padding_len = (4 - (len(content) % 4)) % 4  # outer %4 for case where len(content) % 4 == 0
+        padded_content = content + b'\x00' * padding_len
+
+        block_length = (4    # block type
+                        + 4  # block length
+                        + len(padded_content)
+                        + 4  # block length
+                        )
+
+        block = struct.pack('<LL', block_type, block_length)
+        block += padded_content
+        block += struct.pack('<L', block_length)
+
+        return block
